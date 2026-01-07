b5c6ed34829b32f285fb63b1cefe34a60322b1bb
amadansi26-cmyk <amadansi26@bentleyschool.org>
2026-01-06 18:47:02 -0800

    Update CSP_4_01_Basics_of_Loops.py

--- CSP_4_01_Basics_of_Loops.py
+++ CSP_4_01_Basics_of_Loops.py
@@ -1,105 +1,123 @@
-#All questions must use a loop for full points.
-
-def oddNumbers(n:int) ->str:
+def oddNumbers(n:int) -> str:
     number_string = ""
-    for i in range(1,n):
+    if n < 1:
+        return ""
+    for i in range(1, n + 1):
         if i % 2 != 0:
-            number_string = number_string + " " + str(i)
+            if number_string == "":
+                number_string = str(i)
+            else:
+                number_string = number_string + " " + str(i)
     return number_string
-print(oddNumbers(10))
 
-def backwards(n)-> int:
+
+def backwards(n) -> str:
     rewind = ""
+    if n < 1:
+        return ""
     for i in range(n, 0, -1):
-        rewind = rewind + str(i) + " "
+        if rewind == "":
+            rewind = str(i)
+        else:
+            rewind = rewind + " " + str(i)
     return rewind
-print(backwards(8))
 
-def randomRepeating(n):
+
+def randomRepeating():
     import random
-    highest = 1
-    lowest = 100
-    for i in range(n):
-        randomNum = random.randint(1, 100)
-        if randomNum > highest:
-            highest = randomNum
-        if randomNum < lowest:
-            lowest = randomNum
-    print(highest, lowest)
-randomRepeating(5)
+    tries = 0
+    num = 0
+    while num != 10:
+        num = random.randint(1, 10)
+        tries += 1
+    print(f"it took {tries} tries to get a 10")
+
 
 def randomRange(n):
-    def randomRange(n):
-        counting = 0
-        lowest = 100
-        highest = 1
+    import random
+    if n <= 0:
+        return
+    lowest = 100
+    highest = 1
+    count = 0
 
-        while counting < n:
-            num = random.randint(1, 100)
-            print(num)
+    while count < n:
+        num = random.randint(1, 100)
+        if num < lowest:
+            lowest = num
+        if num > highest:
+            highest = num
+        count += 1
 
-            if num < lowest:
-                lowest = num
-            if num > highest:
-                highest = num
+    print("Lowest number:", lowest)
+    print("Highest number:", highest)
 
-            counting = count + 1
 
-        print("Lowest number:", lowest)
-        print("Highest number:", highest)
-
-def reverse(word:str)->str:
+def reverse(word: str) -> str:
     reverseString = ""
-    for i in range(len(word)-1, -1, -1):
+    for i in range(len(word) - 1, -1, -1):
         reverseString = reverseString + word[i]
-    print(reverseString)
-reverse("zeug")
+    return reverseString
+
 
 def fizzBuzzContinuous(n):
     result = ""
+    if n < 1:
+        return ""
     m = 1
     while m <= n:
         if m % 3 == 0 and m % 5 == 0:
-            result = result + "fizzbuzz "
+            result = result + "fizzbuzz"
         elif m % 3 == 0:
-            result = result + "fizz "
+            result = result + "fizz"
         elif m % 5 == 0:
-            result = result + "buzz "
+            result = result + "buzz"
         else:
-            result = result + str(m) + " "
-        m = m + 1
+            result = result + str(m)
+
+        if m != n:
+            result = result + " "
+        m += 1
     return result
 
+
 def collatz(n):
-    while n > 1:
+    if n < 1:
+        return ""
+    result = ""
+    while True:
+        result = result + str(n)
+        if n == 1:
+            break
+        result = result + " "
         if n % 2 == 0:
-            print(n)
-            n = n/2
-            continue
-        if n % 2 != 0:
-            print(n)
-            n = (n*3) + 1
-collatz(99)
+            n = n // 2
+        else:
+            n = n * 3 + 1
+    return result
+
 
 def fibonacci(n):
     if n <= 0:
         return ""
 
-    if n == 1:
-        return "0 "
-
-    result = "0 1 "
     a = 0
     b = 1
-    count = 2
+    result = ""
+    count = 0
 
     while count < n:
+        if result == "":
+            result = str(a)
+        else:
+            result = result + " " + str(a)
         c = a + b
-        result = result + str(c) + " "
         a = b
         b = c
-        count = count + 1
+        count += 1
 
     return result
 
+
 print(fibonacci(300))
+
